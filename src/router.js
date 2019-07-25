0a1a3abc8e40bf97264d9f11ddbdb97642d64206
Aron Tolentino <arontolentino@outlook.com>
2019-07-25 00:17:51 -0400

    Create Add New Smoothie view

--- src/router.js
+++ src/router.js
@@ -1,15 +1,21 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Index from "./views/Index";
+import Vue from 'vue';
+import Router from 'vue-router';
+import Index from './views/Index';
+import AddSmoothie from './views/AddSmoothie.vue';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: "/",
-      name: "Index",
+      path: '/',
+      name: 'Index',
       component: Index
+    },
+    {
+      path: '/add-smoothie',
+      name: 'AddSmoothie',
+      component: AddSmoothie
     }
   ]
 });
